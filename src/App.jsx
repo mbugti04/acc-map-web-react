9e3587cfc7f4cfd56ce9eac489cc15831f1f89b3
Mustafa Bugti <mustafammbg@gmail.com>
2024-11-17 10:05:13 -0500

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "leaflet/dist/leaflet.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import MapComponent from './MapComponent';
 import ForumComponent from './ForumComponent';
+import { Container, Navbar, Nav } from 'react-bootstrap';
 
 function App() {
   const [locations, setLocations] = useState([]);
@@ -16,14 +18,32 @@ function App() {
   }, []);
 
   return (
+    <>
+    <Navbar className="bg-body-tertiary" style={{ padding: '0.5rem 1rem', maxHeight: '60px' }}>
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              src="src/assets/ACCMapLogo.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />
+            AccMap
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+    
     <div className="container">
-      <div className="map">
+      
+      <div className="map" id="map">
         <MapComponent locations={locations} />
       </div>
-      <div className="forum">
+      <div className="forum" id="forum">
         <ForumComponent locations={locations} />
       </div>
     </div>
+    </>
   );
 }
 
