7b5a5226deba1a0fe13f5ff4c2aaf809807c16a1
Mustafa Bugti <mustafammbg@gmail.com>
2024-11-17 04:21:37 -0500

    locations get mapped

--- app.py
+++ app.py
@@ -1,10 +1,52 @@
 # app.py
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 import time
 
 app = Flask(__name__)
 CORS(app)
+
+locations = [
+    {
+        "id": 1,
+        "latitude": 41.37937613418269,
+        "longitude": -72.10613281840935,
+        "marker_title": "Location 1",
+        "marker_description": "Description for location 1"
+    },
+    {
+        "id": 2,
+        "latitude": 41.38,
+        "longitude": -72.2,
+        "marker_title": "Location 2",
+        "marker_description": "Description for location 2"
+    },
+    {
+        "id": 3,
+        "latitude": 41.39,
+        "longitude": -72.3,
+        "marker_title": "Location 3",
+        "marker_description": "Description for location 3"
+    }
+]
+
+@app.route('/locations')
+def location():
+    return jsonify(locations)
+
+@app.route('/locations', methods=['POST'])
+def add_location():
+    new_location = request.get_json()
+    new_location['id'] = locations[-1]['id'] + 1 if locations else 1
+    locations.append(new_location)
+    return jsonify(new_location), 201
+
+@app.route('/locations/<int:id>')
+def location_by_id(id):
+    for location in locations:
+        if location['id'] == id:
+            return jsonify(location)
+    return jsonify({"message": "Location not found."})
 
 @app.route('/')
 def index():
--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,33 @@
+import "leaflet/dist/leaflet.css";
 import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
 
 function App() {
-  const [currentTime, setCurrentTime] = useState(0);
+  const [locations, setLocations] = useState([]);
 
   useEffect(() => {
-    fetch('http://127.0.0.1:5001/time').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
+    fetch('http://127.0.0.1:5001/locations').then(res => res.json()).then(data => {
+      setLocations(data);
     });
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>The current time is {currentTime}.</p>
-      </header>
-    </div>
+    <MapContainer center={[41.37, -72.11]} zoom={12} scrollWheelZoom={true} style={{ height: '98vh', width: '98vw'}}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {locations.map(location => (
+        <Marker key={location.id} position={[location.latitude, location.longitude]}>
+          <Popup>
+            <strong>{location.marker_title}</strong><br />{location.marker_description}
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
   );
 }
 
